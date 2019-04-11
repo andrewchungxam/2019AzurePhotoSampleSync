40f0771f49d2d764857685f1d7ddf4029887b94e
ac <andrew.chung@xamarin.com>
2019-04-11 09:40:30 -0400

    updated and deleted all items

--- AzureBlobStorageSampleApp.Shared/Models/FilterValueModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace AzureBlobStorageSampleApp.Shared.Models
-{
-    public class FilterValueModel
-    {
-        public string PropertyToSort { get;set;}
-        public string ValueToSortBy { get;set;}
-    }
-}
